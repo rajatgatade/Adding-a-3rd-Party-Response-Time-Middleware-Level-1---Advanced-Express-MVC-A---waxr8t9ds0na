1364147f8765412bbf567f3dce86ef94ba08797e
rajatgatade <97111225+rajatgatade@users.noreply.github.com>
2023-04-21 15:47:47 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,12 +3,10 @@ const responseTime = require("response-time");
 const app = express();
 
 app.use(express.json());
+app.use(responseTime());
 
-// Write the 3rd party RESPONSE-TIME middleware to get the desired result
-
-// GET endpointfor sending the result
 app.get("/", function (req, res) {
-  res.send("Checing response time");
+  res.send("Checking response time");
 });
 
 module.exports = app;
